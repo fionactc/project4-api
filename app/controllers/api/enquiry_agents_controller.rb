540d43e21a60acaef2e64265f0a6e5686e04567b
Fiona <fionacheung224@gmail.com>
2016-08-02 17:28:01 +0800

    added agents to matched renter query

--- app/controllers/api/enquiry_agents_controller.rb
+++ app/controllers/api/enquiry_agents_controller.rb
@@ -1,4 +1,13 @@
 class Api::EnquiryAgentsController < ApplicationController
+  before_action :authenticate_agent!
 
-
+  # return all matched renters for current_agent
+  def index
+    @matched_renters = []
+    matched_enquiries_agents = current_agent.enquiry_agents
+    matched_enquiries_agents.each do |match|
+      @matched_renters.push(match.enquiry.renter)
+    end
+    @matched_renters
+  end
 end
--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,8 +1,18 @@
 class StaticsController < ApplicationController
   # for testing
   def home
-    region = 'Kowloon'
-    agents = Agent.where("'#{region}' = ANY (regions) ")
-    render json: agents[1].id
+    # Test 1: find agent by region
+    # region = 'Kowloon'
+    # agents = Agent.where("'#{region}' = ANY (regions) ")
+    # render json: agents[1].id
+
+    # Test 2: find matched renters of current agent
+    # @renter = []
+    # matched_enquiries_agents = current_agent.enquiry_agents
+    # matched_enquiries_agents.each do |match|
+    #   @renter.push(match.enquiry.renter)
+    # end
+    # render json: @renter
+
   end
 end
