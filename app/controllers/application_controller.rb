d28c3cdde3ee0298ae5de88de4c14beab9906bcf
twahyuni <ti.wahyuni@gmail.com>
2016-08-08 13:27:19 +0800

    configure parameter login/signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -67,7 +67,7 @@ class ApplicationController < ActionController::Base
 protected
 
   def configure_permitted_parameters
-    added_attrs = [:email, :password, :password_confirmation, :mobile_number]
+    added_attrs = [:email, :password, :password_confirmation, :mobile_number, :salutation, :first_name, :family_name]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
