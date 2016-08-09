16084f96226e11e96f70360abc952a5e1b29c69f
twahyuni <ti.wahyuni@gmail.com>
2016-08-09 20:09:53 +0800

    rating template

--- app/controllers/api/agent_ratings_controller.rb
+++ app/controllers/api/agent_ratings_controller.rb
@@ -1,10 +1,9 @@
 class Api::AgentRatingsController < ApplicationController
   before_action :authenticate_renter!, except: [:index, :show]
   before_action :set_agent_rating, only: [:show, :update]
-  before_action :set_agent, only: [:create]
 
   def index
-    @agent_ratings = AgentRating.all
+    @agent_ratings = AgentRating.includes(:appointment, :renter, :agent).all
   end
 
   def show
@@ -22,6 +21,8 @@ class Api::AgentRatingsController < ApplicationController
 
   def update
     @agent_rating.assign_attributes(agent_rating_params)
+    @agent_rating.update_attributes(done: true)
+
     @agent = Agent.find_by_id(@agent_rating.agent_id)
 
     if @agent_rating.save
@@ -43,19 +44,15 @@ class Api::AgentRatingsController < ApplicationController
 private
 
   def set_agent_rating
-    @agent_rating = AgentRating.find_by_id(params[:id])
+    @agent_rating = AgentRating.includes(:appointment, :renter, :agent).find_by_id(params[:id])
     if @agent_rating.nil?
       render json: {message: "Cannot find agent_rating with ID #{params[:id]}"}
     end
   end
 
-  def set_agent
-    @agent = Agent.find_by_id(@agent_rating.agent_id)
-  end
-
   # refer to schema
   def agent_rating_params
-    params.permit(:ar_overall_star, :ar_commment, :agent_id, :renter_id, :appointment_id)
+    params.permit(:ar_overall_star, :ar_commment, :agent_id, :renter_id, :appointment_id, :done)
   end
 
 end
--- app/controllers/api/renter_ratings_controller.rb
+++ app/controllers/api/renter_ratings_controller.rb
@@ -3,14 +3,14 @@ class Api::RenterRatingsController < ApplicationController
   before_action :set_renter_ratings, only:  [:show, :update]
 
   def index
-    @renter_ratings = RenterRating.all
+    @renter_ratings = RenterRating.includes(:appointment, :renter, :agent).all
   end
 
   def show
   end
 
   def create
-    @renter_rating = current_agent.renter_ratings.new(renter_rating_params)
+    @renter_rating = RenterRating.new(renter_rating_params)
     if @renter_rating.save
       render 'show'
     else
@@ -20,7 +20,18 @@ class Api::RenterRatingsController < ApplicationController
 
   def update
     @renter_rating.assign_attributes(renter_rating_params)
+    @renter_rating.update_attributes(done: true)
+
     if @renter_rating.save
+      # if nil set a value, if not average the values
+      if @renter.renter_avg_ratings.nil?
+        @renter.update_attributes(renter_avg_ratings: @renter_rating.rr_overall_star)
+      else
+        arr = [@renter_rating.rr_overall_star, @renter.renter_avg_ratings]
+        avg = arr.inject{ |sum, el| sum + el }.to_f / arr.size
+        @renter.update_attributes(renter_avg_ratings: avg)
+      end
+
       render 'show'
     else
       render json: {errors: @renter_rating.errors.messages}, status: 400
@@ -30,7 +41,7 @@ class Api::RenterRatingsController < ApplicationController
 private
 
   def set_renter_rating
-    @renter_rating = RenterRating.find_by_id(params[:id])
+    @renter_rating = RenterRating.includes(:appointment, :renter, :agent).find_by_id(params[:id])
     if @renter_rating.nil?
       render json: {message: "Cannot find renter_rating with ID #{params[:id]}"}
     end
@@ -38,8 +49,7 @@ private
 
   # refer to schema
   def renter_rating_params
-    params.permit(:rr_overall_star, :rr_comment, :no_show, :spy, :agent_id, :renter_id, :appointment_id)
+    params.permit(:rr_overall_star, :rr_comment, :no_show, :spy, :agent_id, :renter_id, :appointment_id, :done)
   end
-
 
 end
